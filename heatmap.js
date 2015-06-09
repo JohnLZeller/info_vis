19cacb047733471b2f6d44a1fa202d2f9a800e0c
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2015-06-08 22:58:55 -0700

    Update to districts

--- heatmap.js
+++ heatmap.js
@@ -7,6 +7,8 @@ var districtRatings2 = d3.map();
 var datasets = [d3.map(), d3.map(), d3.map()];
 
 var us_map;
+var us;
+var congress;
 var datasetNumber = 1;
 var currentDataset = districtRatings1;
 
@@ -43,17 +45,53 @@ queue()
       swag.set(d.id, d);
     })
     .defer(d3.json, "us_districts.json")
+    .defer(d3.json, "us-congress-10m.json")
+    .defer(d3.json, "us-10m.json")
+    
     //.defer(d3.json, "usa_map.json")
     .awaitAll(loadingCallback);
 
 
 function loadingCallback(error, map) {
   us_map = map[1];
+  congress = map[2];
+  us = map[3];
   drawMap(swag, us_map);
 }
 
 
 function drawMap(dataset, map) {
+  svg.append("defs").append("path")
+        .attr("id", "land")
+        .datum(topojson.object(us, us.objects.land))
+        .attr("d", path);
+
+    svg.append("clipPath")
+        .attr("id", "clip-land")
+      .append("use")
+        .attr("xlink:href", "#land");
+
+    svg.append("g")
+        .attr("class", "districts")
+        .attr("clip-path", "url(#clip-land)")
+      .selectAll("path")
+        .data(topojson.object(congress, congress.objects.districts).geometries)
+      .enter().append("path")
+        .attr("d", path)
+      .append("title")
+        .text(function(d) { return d.id; });
+
+    svg.append("path")
+        .attr("class", "district-boundaries")
+        .attr("clip-path", "url(#clip-land)")
+        .datum(topojson.mesh(congress, congress.objects.districts, function(a, b) { return (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
+        .attr("d", path);
+
+    svg.append("path")
+        .attr("class", "state-boundaries")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("d", path);
+  /*
     svg.append("g")
         .attr("class", "counties")
       .selectAll("path")
@@ -72,6 +110,7 @@ function drawMap(dataset, map) {
         .datum(topojson.mesh(map, map.objects.states, function(a, b) { return a !== b; }))
         .attr("class", "states")
         .attr("d", path);
+        */
 }
 
 
