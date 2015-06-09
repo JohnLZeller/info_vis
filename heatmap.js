de0676d8d7350d95d2553788aa85d2d7b84c36cc
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2015-06-08 17:21:15 -0700

    Get toggling kinda working

--- heatmap.js
+++ heatmap.js
@@ -1,7 +1,13 @@
 var width = 960,
     height = 600;
 
-var rateById = d3.map();
+var districtRatings1 = d3.map();
+var districtRatings2 = d3.map();
+var datasets = [d3.map(), d3.map(), d3.map()];
+
+var us_map;
+var datasetNumber = 1;
+var currentDataset = districtRatings1;
 
 var quantize = d3.scale.quantize()
     .domain([0, .15])
@@ -22,32 +28,58 @@ var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
   .html(function(d) {
-    console.log(JSON.stringify(d));
+    //console.log(JSON.stringify(d));
     return "<strong>Rating:</strong> <span style='color:red'>" + d.id + "</span>";
   });
 
 svg.call(tip);
 
+
+
+dataset1 = d3.tsv("unemployment.tsv", function(error, data) {
+//  x.domain(data.map(function(d) { return d.x; }));
+//  y.domain([0, d3.max(data, function(d) { return d.y; })]);
+});
+
+
 queue()
-    .defer(d3.json, "usa_map.json")
-    .defer(d3.tsv, "unemployment.tsv", function(d) { rateById.set(d.id, +d.rate); })
-    .await(ready);
+    .defer(d3.json, "usa_map.json", function(d) { us_map = d; })
+    .defer(d3.tsv, "unemployment.tsv", function(d) { districtRatings1.set(d.id, +d.rate); })
+    .defer(d3.tsv, "contracts-amounts.tsv", function(d) { datasets[0].set(d.id, +d.cont-ratio); datasets[1].set(d.id, +d.amt-ratio); })
+    .awaitAll(function(a, b) { drawMap(us_map, currentDataset); });
 
-function ready(error, us) {
-  svg.append("g")
-      .attr("class", "counties")
-    .selectAll("path")
-      .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append("path")
-      .attr("class", function(d) { return quantize(rateById.get(d.id)); })
-      .attr("d", path)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
+function drawMap(datasetNumber) {
+    svg.append("g")
+        .attr("class", "counties")
+      .selectAll("path")
+        .data(topojson.feature(map, map.objects.counties).features)
+      .enter().append("path")
+        .attr("class", function(d) { return quantize(dataset.get(d.id)); })
+        .attr("d", path)
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
-  svg.append("path")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "states")
-      .attr("d", path);
+    svg.append("path")
+        .datum(topojson.mesh(map, map.objects.states, function(a, b) { return a !== b; }))
+        .attr("class", "states")
+        .attr("d", path);
 }
+
+//function yolo(error, a, b, c) {
+//  drawMap(us_map, districtRatings1);
+//}
+
+setTimeout(function() { drawMap(us_map, districtRatings1); }, 2000);
+
+d3.select('button').on('click', function() {
+    if ( datasetNumber == 1 ) {
+      datasetNumber = 2;
+    } else {
+      datasetNumber = 1;
+    }
+    currentDataset = datasets[datasetNumber];
+    drawMap(us_map, currentDataset);
+});
+
 
 d3.select(self.frameElement).style("height", height + "px");
